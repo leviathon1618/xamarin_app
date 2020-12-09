a0b7d9b7d2c32adc8fd06b4c975c85c7d3edad10
Levi Delaney <levidelaney2000@gmail.com>
2020-12-09 15:37:12 +1300

    restaurant app

---
+++ Xamarin_Forms___Notes__local_database_/Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string CreatedBy { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
